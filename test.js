c784587f06f43c2069d79f10e79b77281bfc52c0
Jerome Caruso <60622127+jerome0caruso@users.noreply.github.com>
2020-05-07 19:39:22 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,57 @@
+let timesVisited = 0;
+let report = document.getElementById('report');
+
+if(localStorage.timesVisited){
+  timesVisited = parseInt(localStorage.timesVisited)
+}
+timesVisited += 1;
+let trying = 0;
+localStorage.setItem('timesVisited', timesVisited);
+
+const myFunk = (string, ...vals) =>
+    string.reduce((acc, iterator, index) => 
+        (`${acc} <span class="color">${vals[index -1]}</span> ${iterator}`));
+    
+
+
+report.innerHTML = (myFunk`You have been to this page ${timesVisited} times!`);
+
+
+//select box
+$("select[name='choose']").change(function() {
+    $("#genderSpan").html($(this).val());
+});
+
+//radio buttons
+$("input:radio[name='species']").change(function() {
+    if($(this).prop('checked')) {
+        $("#speciesSpan").html($(this).val());
+    }
+  
+});
+//check box
+
+var allChecked = []; //all checked 
+
+$("input:checkbox").change(function() {
+    var value = $(this).val();
+    if($(this).prop("checked")) {
+        allChecked.push(value);
+    }
+    else {//if not checked or remove it
+        var index = allChecked.indexOf(value);//get index in the array
+        allChecked.splice(index, 1);
+    }
+    $("#featureSpan").html("");//clear the space before setting the values for checked
+    for (let i = 0; i < allChecked.length; i++){
+        $("#featureSpan").append(allChecked[i]);
+        console.log("I", i)
+        console.log("len",allChecked.length )
+        if( i < allChecked.length -1)
+          $("#featureSpan").append(", ");
+        else
+        $("#featureSpan").append(" ");
+    }
+
+
+})
